cc72b2d66f79d3006aa615ccc49d8c95de59d2b7
Nick Souvannalay <nicksouvannalay@gmail.com>
2022-11-12 20:55:20 -0500

    feat: adding week 10 assignment(s)

---
+++ Week-10-Returning_Data/mysql-java/src/main/java/projects/service/ProjectService.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package projects.service;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import projects.dao.ProjectDao;
+
+import projects.entity.Project;
+
+/**
+ * @author Nick Sovannalay
+ *
+ */
+public class ProjectService {
+	private ProjectDao projectDao = new ProjectDao();
+	
+
+	/**
+	 * Default constructor: creates an instance of ProjectService.
+	 */
+	public Project addProject(Project project) {
+		return projectDao.insertProject(project);
+		
+	}
+
+
+	public List<Project> fetchAllProjects() {
+		return projectDao.fetchAllProjects();
+	}
+
+	public Project fetchProjectById(Integer projectId) {
+		return projectDao.fetchProjectById(projectId).orElseThrow(() -> new NoSuchElementException("Project with project ID=" + projectId + " does not exist."));
+		
+	}
+}
