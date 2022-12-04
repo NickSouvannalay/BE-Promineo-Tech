3e558bba3c35e04d2026797c1cc73efedf2ba6fa
Nick Souvannalay <nicksouvannalay@gmail.com>
2022-12-03 23:52:24 -0500

    feat:adding data for week 12.

---
+++ Week-12-JUnit_Testing/unit-test-assignment/src/test/java/TestDemoTest.java
@@ -0,0 +1,61 @@
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class TestDemoTest {
+
+	private static TestDemo testDemo;
+	
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		testDemo = new TestDemo();
+	}
+
+	@ParameterizedTest
+	@MethodSource("TestDemoTest#argumentsForAddPositive")
+	void assertThatTwoPositiveNumbersAreAddedCorrectly(int a, int b, int expected, boolean expectException) {
+		
+		if(!expectException) {
+		  assertThat(testDemo.addPositive(a, b)).isEqualTo(expected);
+		} else {
+			assertThatThrownBy(() -> testDemo.addPositive(a, b)).isInstanceOf(IllegalArgumentException.class);
+		}
+	}
+
+	static Stream<Arguments> argumentsForAddPositive() {
+		return Stream.of(
+			// returns a value 
+			Arguments.arguments(2, 4, 6, false),
+			Arguments.arguments(10, 17, 27, false),
+			
+			// throws an error
+			Arguments.arguments(0, 0, 0, true),
+			Arguments.arguments(-2, -4, -6, true),
+			Arguments.arguments(-2, 4, 2, true)
+		);
+	}
+	
+	@Test
+	static void assertThatNumberSquaredIsCorrect() {
+		// given
+		TestDemo mockDemo = spy(testDemo);
+		doReturn(5).when(mockDemo).getRandomInt();
+		
+		// when
+		int fiveSquared = mockDemo.randomNumberSquared();
+		
+		// except
+		assertThat(fiveSquared).isEqualTo(25);
+	}
+	
+}
